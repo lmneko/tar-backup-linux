f8f43ab4b3d84fa0f8185b55d79588341dd5ef24
sand <sand@sand-PC>
2018-02-01 20:56:59 +0800

    添加gpt磁盘安装BIOS模式启动，grub2启动引导用#号注释 修该分区功能

--- linux_tar_backup.sh
+++ linux_tar_backup.sh
@@ -2,31 +2,44 @@
 #2018年01月29日 
 #use tar to backup the whole linux system ,and also recovery with tar 
 #need to use in linux livecd system
-#Partition Table: msdos
-###
+#
+#
 ##########################################################
-DATE=`data +%F`
-SYS_NAME=`cat /etc/centos_release`
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
-export PATH DATE SYS_NAME
-read -p "Please input the part of linux system . example : /dev/sda" system_part
-
-    mount /dev/sda2 /mnt
-    mount /dev/sda1 /mnt/boot
+export PATH 
+DATE=`data +%F`
+START_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+echo "It is best to use this script in the LiveCD system."
+echo "And it is only to use in msdos part table."
+read -p "Please input the part of linux system . example : \" /dev/sda \" : " system_part
+if [ ! -b $sel_disk ] ; then
+        echo "The selected disk is not exist. " ; exit 1
+fi  
+read -p "Please input the directory to save backup file : " dir_bak   
+if [ ! -d $dir_bak ] ; then
+        echo "The selected directory is not exist. " ; exit 1
+fi  
+    mount ${sel_disk} /mnt
+    mount ${sel_disk}1 /mnt/boot
     mount -o bind /dev /mnt/dev
     mount -o bind /sys  /mnt/sys
     mount -o bind /proc /mnt/proc
     chroot /mnt
-    tar --xattrs -cvpzf /mnt/d/${SYS_NAME}_backup_${DATE}.tar.gz \
+    tar --xattrs -cvpzf ${dir_bak}/system_backup_${DATE}.tar.gz \
     --exclude=/proc \
     --exclude=/lost+found \
     --exclude=/mnt \
     --exclude=/sys \
     --exclude=/media \
-    --exclude=/home \
+#    --exclude=/home \
     --exclude=/run \
     / 
-exit
+    exit
+COM_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+echo "Backup successful!"
+echo "Start time : ${START_TIME}"
+echo "Complete time : ${COM_TIME}"
+
 
 
 
--- linux_tar_recovery.sh
+++ linux_tar_recovery.sh
@@ -1,14 +1,56 @@
 #!/bin/bash
-#2018Äê01ÔÂ29ÈÕ 
+#2018-01-29 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
-read -p "select a empty disk to recovery. example : /dev/sda" sel_disk
-read -p "select the backup file. exapmle: *backup_2018_1_30.tar.gz" backup_file
-parted ${seldisk} -s -a optimal mklabel msdos mkpart primary 1m 1G  mkpart primary linux-swap 1G 9G mkpart primary 9G 100% toggle 1 boot
+START_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+BOOT_SIZE=1000                #size MB
+SWAP_SIZE=8000                
+# Check that we are root
+(( EUID != 0 )) && exec sudo -- "$0" "$@"
+#if [ "$EUID" -ne 0 ]; then
+	#echo "Please run as root"
+	#exit 1SEL_
+#fi
+read -p "Select a empty disk to recovery. example : \" /dev/sda \" : " sel_disk
+
+if [ ! -b ${sel_disk} ] ; then
+        echo "The selected disk is not exist. " ; exit 1
+fi     
+ROOT_START=$((${BOOT_SIZE}+${SWAP_SIZE}+2))
+parted ${sel_disk} -s -a optimal mklabel msdos \
+mkpart primary  2 $(($BOOT_SIZE+2)) \
+mkpart primary linux-swap $(($BOOT_SIZE+2)) ${ROOT_START} \
+mkpart primary ${ROOT_START} 100% \
+toggle 1 boot 
+if [ $? == 0 ]; then
+        partprobe 
+        echo "Making disk \"${sel_disk}\" partition is successful"
+        parted -s ${sel_disk} p | tail -n 5 | grep -v "^$" 
+        else
+        echo "parted disk ${sel_disk} error and exit..." ; exit 1
+fi
+#When using the BIOS boot, install grub2 on the GPT partition table
+###################################################################
+#ROOT_START=$(($BOOT_SIZE+$SWAP_SIZE+4))
+#parted ${sel_disk} -s -a optimal mklabel gpt \
+#mkpart primary  2 4 \
+#mkpart primary  4 $(($BOOT_SIZE+4)) \
+#mkpart primary linux-swap $(($BOOT_SIZE+4)) ${ROOT_START}  \
+#mkpart primary ${ROOT_START} 100%  \
+#set 1 bios_grub on \
+#toggle 2 boot \
+#|| echo "parted disk ${sel_disk} error and exit..." ; exit 1
+
 mkfs.ext4 ${sel_disk}3 && mount ${sel_disk}3 /mnt
 mkfs.ext4 ${sel_disk}1 && mount ${sel_disk}1 /mnt/boot
 mkswap ${sel_disk}2 && swapon ${sel_disk}2
 
+read -p "select the backup file. exapmle: *backup_2018_1_30.tar.gz : " backup_file
+if [ ! -f $backup_file] ; then
+        echo "backup file is not exist,exiting..." ; exit 1
+fi  
+
+echo "Restore files from the backup file and install boot loader ."
 tar --xattrs -xpf ~/$backup_file -C /mnt 
 mkdir -p /mnt/{dev,proc,sys}
 mount -o bind /dev /mnt/dev
@@ -28,4 +70,5 @@ mv /etc/fstab /etc/fstab.bak
 echo "UUID=$boot_uuid    /boot        $boot_fstype    defaults    0 0" >> /etc/fstab
 echo "${sel_disk}3    /       $root_fstype        defaults    0 0" >> /etc/fstab
 echo "${sel_disk}2    swap        defaults    0 0"  >> /etc/fstab
-mount -a && echo "Recovery system successful ,check file "/etc/fstab" and reboot " ; exit  ||  exit
+mount -a && echo "Recovery system successful ,check file "/etc/fstab" and reboot " ; exit  ||  exit 1
+
