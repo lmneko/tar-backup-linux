e0650a2f37603a9441e93ce7f55fe56afc31fbf0
root <root@DESKTOP-IT0AJN0.localdomain>
2018-02-02 10:52:37 +0800

    Changes to be committed: 	modified:   linux_tar_backup.sh         check and test

--- linux_tar_backup.sh
+++ linux_tar_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#2018年01月29日 
+#2018-01-29 
 #use tar to backup the whole linux system ,and also recovery with tar 
 #need to use in linux livecd system
 #
@@ -7,38 +7,41 @@
 ##########################################################
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH 
-DATE=`data +%F`
+DATE=`date +%F`
 START_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+MNT_DIR='/mnt'
+extend_opt=' '
+# Check that we are root
+(( EUID != 0 )) && exec sudo -- "$0" "$@"
 echo "It is best to use this script in the LiveCD system."
 echo "And it is only to use in msdos part table."
 read -p "Please input the part of linux system . example : \" /dev/sda \" : " system_part
 if [ ! -b $sel_disk ] ; then
         echo "The selected disk is not exist. " ; exit 1
 fi  
-read -p "Please input the directory to save backup file : " dir_bak   
-if [ ! -d $dir_bak ] ; then
-        echo "The selected directory is not exist. " ; exit 1
+read -p "Please input the directory 
+that best choice of larger than 10G directory to save the backup file : " dir_bak   
+if [  ! -d $dir_bak ] ; then
+        echo "The selected directory is not exist. " 
+        exit
 fi  
-    mount ${sel_disk} /mnt
-    mount ${sel_disk}1 /mnt/boot
-    mount -o bind /dev /mnt/dev
-    mount -o bind /sys  /mnt/sys
-    mount -o bind /proc /mnt/proc
-    chroot /mnt
+    mount ${sel_disk} ${MNT_DIR} && mount ${sel_disk}1 ${MNT_DIR}/boot  \
+    || echo "mount error! and exit..." ; exit 
+    echo "Backup .Please wait..."
+    sleep 2
     tar --xattrs -cvpzf ${dir_bak}/system_backup_${DATE}.tar.gz \
-    --exclude=/proc \
-    --exclude=/lost+found \
-    --exclude=/mnt \
-    --exclude=/sys \
-    --exclude=/media \
-#    --exclude=/home \
-    --exclude=/run \
-    / 
-    exit
-COM_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+    --exclude=${MNT_DIR}/proc \
+    --exclude=${MNT_DIR}/lost+found \
+    --exclude=${MNT_DIR}/mnt \
+    --exclude=${MNT_DIR}/sys \
+    --exclude=${MNT_DIR}/media \
+    --exclude=${MNT_DIR}/run \
+    ${MNT_DIR}
+    
+END_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 echo "Backup successful!"
 echo "Start time : ${START_TIME}"
-echo "Complete time : ${COM_TIME}"
+echo "Complete time : ${END_TIME}"
 
 
 
