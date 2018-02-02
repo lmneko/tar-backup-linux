35c23606c383978bba152828b2aeee3ff4e437c8
root <root@DESKTOP-IT0AJN0.localdomain>
2018-02-02 09:51:36 +0800

    check  and run complete it

--- linux_tar_recovery.sh
+++ linux_tar_recovery.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #2018-01-29 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
-export PATH
+export PATH 
 START_TIME=`date '+%Y-%m-%d %H:%M:%S'`
 BOOT_SIZE=1000                #size MB
-SWAP_SIZE=8000                
+SWAP_SIZE=8000
+boot_bios_efi='bios'
+boot_lvm_disk='disk'
+ROOT_START=$((${BOOT_SIZE}+${SWAP_SIZE}+2))                
 # Check that we are root
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
 #if [ "$EUID" -ne 0 ]; then
@@ -15,15 +18,14 @@ read -p "Select a empty disk to recovery. example : \" /dev/sda \" : " sel_disk
 
 if [ -b ${sel_disk} ] ; then
 	sfdisk -d ${sel_disk} > seldisk_partion_table.bak && \
-	echo " \"$sel_disk\"\'s partion table has been backed up to the file seldisk_partion_table.bak"     #backup partion table
+	echo " \"$sel_disk\"'s partion table has been backed up to the file seldisk_partion_table.bak"     #backup partion table
 else
         echo "The selected disk is not exist. " ; exit 1
 fi
 
-ROOT_START=$((${BOOT_SIZE}+${SWAP_SIZE}+2))
 parted ${sel_disk} -s -a optimal mklabel msdos \
 mkpart primary  2 $(($BOOT_SIZE+2)) \
-mkpart primary linux-swap $(($BOOT_SIZE+2)) ${ROOT_START} \
+mkpart primary $(($BOOT_SIZE+2))  ${ROOT_START}  \
 mkpart primary ${ROOT_START} 100% \
 toggle 1 boot 
 if [ $? == 0 ]; then
@@ -31,13 +33,8 @@ if [ $? == 0 ]; then
         echo "Making disk \"${sel_disk}\" partition is successful"
         parted -s ${sel_disk} p | tail -n 5 | grep -v "^$" 
 else
-        read -p "parted disk ${sel_disk} error. Whether to restore the partition table ? [yes/no]:" rectable_confirm
-	if [ ${rectable_confirm} == '[Yy][Ee][Ss]' ] || [ ${rectable_confirm} == '[Yy]' ]; then
+        echo "parted disk ${sel_disk} error.  Restore the partition table ." 
 		sfdisk ${sel_disk} < seldisk_partion_table.bak
-		exit 
-	else
-		exit
-	fi
 fi
 
 
@@ -58,29 +55,37 @@ mkfs.ext4 ${sel_disk}1 && mount ${sel_disk}1 /mnt/boot
 mkswap ${sel_disk}2 && swapon ${sel_disk}2
 
 read -p "select the backup file. exapmle: *backup_2018_1_30.tar.gz : " backup_file
-if [ ! -f $backup_file] ; then
-        echo "backup file is not exist,exiting..." ; exit 1
+if [ ! -f $backup_file ] ; then
+        echo "backup file is not exist,exiting..." 
+        exit 1
 fi  
 
-echo "Restore files from the backup file and install boot loader ."
-tar --xattrs -xpf ~/$backup_file -C /mnt 
+echo "Restore files from the backup file and install boot loader . Please wait..."
+tar --xattrs -xpf $backup_file -C /mnt 
+echo "Restore completed."
 mkdir -p /mnt/{dev,proc,sys}
 mount -o bind /dev /mnt/dev
 mount -o bind /sys /mnt/sys
 mount -o bind /proc /mnt/proc
+export sel_disk
 chroot /mnt
-grub2-install --target=i386-pc --recheck --boot-directory=/boot /dev/sda && \
+grub2-install --target=i386-pc --recheck --boot-directory=/boot ${sel_disk} && \
            grub2-mkconfig -o /boot/grub2/grub.cfg
 
-$boot_uuid=`blkid | grep ${sel_disk}1 | cut -d \" -f2`
-$boot_fstype=`blkid | grep ${sel_disk}1 | cut -d \" -f4`
-$root_uuid=`blkid | grep ${sel_disk}3 | cut -d \" -f2`
-$root_fstype=`blkid | grep ${sel_disk}3 | cut -d \* -f4`
-$swap_uuid=`blkid | grep ${sel_disk}2 | cut -d \" -f2`
-$swap_fstype=swap
+boot_uuid=`blkid | grep ${sel_disk}1 | cut -d \" -f2`
+boot_fstype=`blkid | grep ${sel_disk}1 | cut -d \" -f4`
+root_uuid=`blkid | grep ${sel_disk}3 | cut -d \" -f2`
+root_fstype=`blkid | grep ${sel_disk}3 | cut -d \" -f4`
+swap_uuid=`blkid | grep ${sel_disk}2 | cut -d \" -f2`
+swap_fstype=swap
 mv /etc/fstab /etc/fstab.bak
 echo "UUID=$boot_uuid    /boot        $boot_fstype    defaults    0 0" >> /etc/fstab
 echo "${sel_disk}3    /       $root_fstype        defaults    0 0" >> /etc/fstab
-echo "${sel_disk}2    swap        defaults    0 0"  >> /etc/fstab
-mount -a && echo "Recovery system successful ,check file "/etc/fstab" and reboot " ; exit  ||  exit 1
+echo "${sel_disk}2    swap     swap    defaults    0 0"  >> /etc/fstab
+mount -a && echo "Recovery system successful ,check file "/mnt/etc/fstab" and reboot "
+exit
+END_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+echo "Start time  : $START_TIME"
+echo "End time : $END_TIME"
+
 
